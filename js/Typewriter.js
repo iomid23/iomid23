67a8cd6b4853aa6ec431a63acf3d440d3f69f6c7
Omid Amini <97903257+iomid23@users.noreply.github.com>
2023-07-03 15:56:34 +0200

    Update Typewriter.js
    fix bugs

--- js/Typewriter.js
+++ js/Typewriter.js
@@ -1,37 +1,40 @@
-var aText = new Array(
-    "hi, i'm ron", // add your text
-    "a web developer", // add your text
-    "based in riga, latvia", // add your text
-    "i drink lot of tea", // add your text
-    "and build beautiful websites" // add your text
-    );
-    var iSpeed = 100; 
-    var iIndex = 0; 
-    var iArrLength = aText[0].length; 
-    var iScrollAt = 20; 
-     
-    var iTextPos = 0; 
-    var sContents = ''; 
-    var iRow; 
-     
-    function typewriter()
-    {
-     sContents =  ' ';
-     iRow = Math.max(0, iIndex-iScrollAt);
-     var destination = document.getElementById("typedtext");
-     
-     while ( iRow < iIndex ) {
-      sContents += aText[iRow++] + '<br />';
-     }
-     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-     if ( iTextPos++ == iArrLength ) {
-      iTextPos = 0;
-      iIndex++;
-      if ( iIndex != aText.length ) {
-       iArrLength = aText[iIndex].length;
-       setTimeout("typewriter()", 500);
-      }
-     } else {
-      setTimeout("typewriter()", iSpeed);
-     }
+const aText = [
+  "hi, i'm ron", // add your text
+  "a web developer", // add your text
+  "based in riga, latvia", // add your text
+  "i drink a lot of tea", // add your text
+  "and build beautiful websites" // add your text
+];
+const iSpeed = 100; 
+let iIndex = 0; 
+let iArrLength = aText[0].length; 
+const iScrollAt = 20; 
+let iTextPos = 0; 
+let sContents = ''; 
+let iRow; 
+
+function typewriter() {
+  sContents = '';
+  iRow = Math.max(0, iIndex - iScrollAt);
+  const destination = document.getElementById("typedtext");
+
+  while (iRow < iIndex) {
+    sContents += aText[iRow++] + '<br />';
+  }
+
+  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+
+  if (iTextPos++ === iArrLength) {
+    iTextPos = 0;
+    iIndex++;
+
+    if (iIndex !== aText.length) {
+      iArrLength = aText[iIndex].length;
+      setTimeout(typewriter, 500);
     }
+  } else {
+    setTimeout(typewriter, iSpeed);
+  }
+}
+
+typewriter();
